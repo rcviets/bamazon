1eaa251fbfca564c8ecd768a17d986fd8998f5ff
Randamus <rcviets@gmail.com>
2019-10-31 19:16:26 -0600

    read all products function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,5 +10,27 @@ let connection = mysql.createConnection ({
     port: 3306,
     user: 'root',
     password: 'Plzgtf0k!',
-    database: 'bamazon'
+    database: 'bamazon_db'
 });
+
+// Check Connection to Database
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to Bamazon');
+    readProducts();
+    connection.end();
+});
+
+// Read the Products Table
+
+function readProducts() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + ' | ' + res[i].product_name + ' | ' +
+                res[i].department_name + ' | ' + res[i].price + ' | ' + res[i].stock_quantity);
+        }
+        console.log('++++++++++++++++++++++++++++++');
+    });
+}
